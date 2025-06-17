8d147a2c648aea7952ca6f56fc1fd6a6b6852e16
unknown <basteiroabril@gmail.com>
2025-06-17 19:13:38 -0300

    Cambio estilos en creacion de pregunta

--- frontend/src/app/components/creacion-encuesta/creacion-encuesta.component.ts
+++ frontend/src/app/components/creacion-encuesta/creacion-encuesta.component.ts
@@ -2,12 +2,12 @@ import { Component, inject, signal } from '@angular/core';
 import { InputTextModule } from 'primeng/inputtext';
 import { FloatLabel } from 'primeng/floatlabel';
 import {
-  FormArray,
-  FormControl,
-  FormGroup,
-  FormsModule,
-  ReactiveFormsModule,
-  Validators,
+    FormArray,
+    FormControl,
+    FormGroup,
+    FormsModule,
+    ReactiveFormsModule,
+    Validators,
 } from '@angular/forms';
 import { SeccionComponent } from '../seccion/seccion.component';
 import { ButtonModule } from 'primeng/button';
@@ -15,170 +15,172 @@ import { GestionPreguntaDialogComponent } from '../gestion-pregunta-dialog/gesti
 import { PreguntaDTO } from '../../interfaces/pregunta.dto';
 import { ConfirmationService, MessageService } from 'primeng/api';
 import {
-  tiposPreguntaPresentacion,
-  TiposRespuestaEnum,
+    tiposPreguntaPresentacion,
+    TiposRespuestaEnum,
 } from '../../enums/tipos-pregunta.enum';
 import { TextoErrorComponent } from '../texto-error/texto-error.component';
 import { EncuestasService } from '../../services/encuestas.service';
 import { Router } from '@angular/router';
 import { CreateEncuestaDTO } from '../../interfaces/create-encuesta.dto';
+import { CardModule } from 'primeng/card';
 
 @Component({
-  selector: 'app-creacion-encuesta',
-  imports: [
-    InputTextModule,
-    FloatLabel,
-    FormsModule,
-    SeccionComponent,
-    ButtonModule,
-    GestionPreguntaDialogComponent,
-    ReactiveFormsModule,
-    TextoErrorComponent,
-  ],
-  templateUrl: './creacion-encuesta.component.html',
-  styleUrl: './creacion-encuesta.component.css'
+    selector: 'app-creacion-encuesta',
+    imports: [
+        InputTextModule,
+        FloatLabel,
+        FormsModule,
+        SeccionComponent,
+        ButtonModule,
+        GestionPreguntaDialogComponent,
+        ReactiveFormsModule,
+        TextoErrorComponent,
+        CardModule
+    ],
+    templateUrl: './creacion-encuesta.component.html',
+    styleUrl: './creacion-encuesta.component.css'
 })
 export class CreacionEncuestaComponent {
-  form: FormGroup;
+    form: FormGroup;
 
-  private messageService: MessageService = inject(MessageService);
+    private messageService: MessageService = inject(MessageService);
 
-  private router: Router = inject(Router);
+    private router: Router = inject(Router);
 
-  private confirmationService: ConfirmationService =
-    inject(ConfirmationService);
+    private confirmationService: ConfirmationService =
+        inject(ConfirmationService);
 
-  private encuestasService: EncuestasService = inject(EncuestasService);
+    private encuestasService: EncuestasService = inject(EncuestasService);
 
-  dialogGestionPreguntaVisible = signal<boolean>(false);
+    dialogGestionPreguntaVisible = signal<boolean>(false);
 
-  preguntaSeleccionada = signal<PreguntaDTO | null>(null);
+    preguntaSeleccionada = signal<PreguntaDTO | null>(null);
 
-  constructor() {
-    this.form = new FormGroup({
-      nombre: new FormControl<string>('', Validators.required),
-      preguntas: new FormArray<FormControl<PreguntaDTO>>(
-        [],
-        [Validators.required, Validators.minLength(1)]
-      ),
-    });
-  }
-
-  get preguntas(): FormArray<FormControl<PreguntaDTO>> {
-    return this.form.get('preguntas') as FormArray<FormControl<PreguntaDTO>>;
-  }
-
-  get nombre(): FormControl<string> {
-    return this.form.get('nombre') as FormControl<string>;
-  }
-
-  abrirDialog() {
-    this.dialogGestionPreguntaVisible.set(true);
-  }
-
-  agregarPregunta(pregunta: PreguntaDTO) {
-    this.preguntas.push(
-      new FormControl<PreguntaDTO>(pregunta) as FormControl<PreguntaDTO>
-    );
-  }
-
-  eliminarPregunta(index: number) {
-    this.preguntas.removeAt(index);
-  }
-
-  getTipoPreguntaPresentacion(tipo: TiposRespuestaEnum): string {
-    return tiposPreguntaPresentacion.find(
-      (tipoPresentacion) => tipoPresentacion.tipo === tipo
-    )!?.presentacion;
-  }
-
-  confirmarCrearEncuesta() {
-    this.confirmationService.confirm({
-      message: 'Confirmar creacion de encuesta?',
-      header: 'confirmacion',
-      closable: true,
-      closeOnEscape: true,
-      icon: 'pi pi-exclamation-triangle',
-      rejectButtonProps: {
-        label: 'Cancelar',
-        severity: 'secondary',
-        outlined: true,
-      },
-      acceptButtonProps: {
-        label: 'Confirmar',
-      },
-      accept: () => {
-        this.crearEncuesta();
-      },
-    });
-  }
-
-  confirmarEliminarPregunta(index:number) {
-    this.confirmationService.confirm({
-      message: 'Confirmar eliminacion?',
-      header: 'Confirmacion',
-      closable: true,
-      closeOnEscape: true,
-      icon: 'pi pi-exclamation-triangle',
-      rejectButtonProps: {
-        label: 'Cancelar',
-        severity: 'secondary',
-        outlined: true,
-      },
-      acceptButtonProps: {
-        label: 'Confirmar',
-      },
-      accept: () => {
-        this.eliminarPregunta(index);
-      },
-    });
-  }
-
-  crearEncuesta() {
-    if (!this.form.valid) {
-      this.form.markAllAsTouched();
-      this.messageService.add({
-        severity: 'error',
-        summary: 'Hay errores en el formulario',
-      });
-      return;
-    }
-
-    const encuesta: CreateEncuestaDTO = this.form.value;
-
-    for (let i = 0; i < encuesta.preguntas.length; i++) {
-      const pregunta = encuesta.preguntas[i];
-      pregunta.numero = i + 1;
-
-      if (pregunta.opciones) {
-        for (let j = 0; j < pregunta.opciones.length; j++) {
-          pregunta.opciones[j].numero = j +1;
-        }
-      }
-    }
-
-    this.encuestasService.crearEncuesta(encuesta).subscribe({
-      next: (res) => {
-        this.messageService.add({
-          severity: 'success',
-          summary: 'La encuesta se creo con exito',
+    constructor() {
+        this.form = new FormGroup({
+            nombre: new FormControl<string>('', Validators.required),
+            preguntas: new FormArray<FormControl<PreguntaDTO>>(
+                [],
+                [Validators.required, Validators.minLength(1)]
+            ),
         });
+    }
 
-        this.router.navigateByUrl(
-          '/presentacion-enlaces?id-encuesta=' +
-          res.id +
-          '&codigo-respuesta=' +
-          res.codigoRespuesta +
-          '&codigo-resultados=' +
-          res.codigoResultados
+    get preguntas(): FormArray<FormControl<PreguntaDTO>> {
+        return this.form.get('preguntas') as FormArray<FormControl<PreguntaDTO>>;
+    }
+
+    get nombre(): FormControl<string> {
+        return this.form.get('nombre') as FormControl<string>;
+    }
+
+    abrirDialog() {
+        this.dialogGestionPreguntaVisible.set(true);
+    }
+
+    agregarPregunta(pregunta: PreguntaDTO) {
+        this.preguntas.push(
+            new FormControl<PreguntaDTO>(pregunta) as FormControl<PreguntaDTO>
         );
-      },
-      error: (err) => {
-        this.messageService.add({
-          severity: 'error',
-          summary: 'Ha ocurrido un error al crear la encuesta',
+    }
+
+    eliminarPregunta(index: number) {
+        this.preguntas.removeAt(index);
+    }
+
+    getTipoPreguntaPresentacion(tipo: TiposRespuestaEnum): string {
+        return tiposPreguntaPresentacion.find(
+            (tipoPresentacion) => tipoPresentacion.tipo === tipo
+        )!?.presentacion;
+    }
+
+    confirmarCrearEncuesta() {
+        this.confirmationService.confirm({
+            message: 'Confirmar creación de encuesta?',
+            header: 'Guardar encuesta',
+            closable: true,
+            closeOnEscape: true,
+            icon: 'pi pi-exclamation-triangle',
+            rejectButtonProps: {
+                label: 'Cancelar',
+                severity: 'secondary',
+                outlined: true,
+            },
+            acceptButtonProps: {
+                label: 'Confirmar',
+            },
+            accept: () => {
+                this.crearEncuesta();
+            },
         });
-      },
-    });
-  }
+    }
+
+    confirmarEliminarPregunta(index: number) {
+        this.confirmationService.confirm({
+            message: 'Confirmar eliminacion?',
+            header: 'Confirmacion',
+            closable: true,
+            closeOnEscape: true,
+            icon: 'pi pi-exclamation-triangle',
+            rejectButtonProps: {
+                label: 'Cancelar',
+                severity: 'secondary',
+                outlined: true,
+            },
+            acceptButtonProps: {
+                label: 'Confirmar',
+            },
+            accept: () => {
+                this.eliminarPregunta(index);
+            },
+        });
+    }
+
+    crearEncuesta() {
+        if (!this.form.valid) {
+            this.form.markAllAsTouched();
+            this.messageService.add({
+                severity: 'error',
+                summary: 'Hay errores en el formulario',
+            });
+            return;
+        }
+
+        const encuesta: CreateEncuestaDTO = this.form.value;
+
+        for (let i = 0; i < encuesta.preguntas.length; i++) {
+            const pregunta = encuesta.preguntas[i];
+            pregunta.numero = i + 1;
+
+            if (pregunta.opciones) {
+                for (let j = 0; j < pregunta.opciones.length; j++) {
+                    pregunta.opciones[j].numero = j + 1;
+                }
+            }
+        }
+
+        this.encuestasService.crearEncuesta(encuesta).subscribe({
+            next: (res) => {
+                this.messageService.add({
+                    severity: 'success',
+                    summary: 'La encuesta se creo con exito',
+                });
+
+                this.router.navigateByUrl(
+                    '/presentacion-enlaces?id-encuesta=' +
+                    res.id +
+                    '&codigo-respuesta=' +
+                    res.codigoRespuesta +
+                    '&codigo-resultados=' +
+                    res.codigoResultados
+                );
+            },
+            error: (err) => {
+                this.messageService.add({
+                    severity: 'error',
+                    summary: 'Ha ocurrido un error al crear la encuesta',
+                });
+            },
+        });
+    }
 }
