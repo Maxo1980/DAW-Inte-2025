ec90d00808f544ebafb60e98473f2de8f9257940
unknown <basteiroabril@gmail.com>
2025-06-25 18:03:55 -0300

    protejo ruta presentacion-enlaces

--- frontend/src/app/components/presentacion-enlaces/presentacion-enlaces.component.ts
+++ frontend/src/app/components/presentacion-enlaces/presentacion-enlaces.component.ts
@@ -2,7 +2,7 @@ import { Component, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { ActivatedRoute } from '@angular/router';
 import { TooltipModule } from 'primeng/tooltip';
 import { PopoverModule } from 'primeng/popover';
@@ -11,7 +11,7 @@ import { PresentacionQRComponent } from '../presentacion-qr/presentacion-qr.comp
 @Component({
     selector: 'app-presentacion-enlaces',
     standalone: true,
-    imports: [CommonModule, CardModule, ButtonModule, RouterModule, TooltipModule, PopoverModule,PresentacionQRComponent  ],
+    imports: [CommonModule, CardModule, ButtonModule, RouterModule, TooltipModule, PopoverModule, PresentacionQRComponent],
     templateUrl: './presentacion-enlaces.component.html',
     styleUrls: ['./presentacion-enlaces.component.css'],
 })
@@ -21,7 +21,7 @@ export class PresentacionEnlacesComponent {
     enlaceResultados = '';
     mostrarPopover = false;
 
-    constructor(private route: ActivatedRoute) {
+    constructor(private route: ActivatedRoute, private router: Router) {
         const id = this.route.snapshot.queryParamMap.get('id-encuesta');
         const codigoRespuesta = this.route.snapshot.queryParamMap.get('codigo-respuesta');
         const codigoResultados = this.route.snapshot.queryParamMap.get('codigo-resultados');
@@ -29,9 +29,10 @@ export class PresentacionEnlacesComponent {
         if (id && codigoRespuesta && codigoResultados) {
             this.enlaceRespuesta = `http://localhost:4200/encuestas/${id}?codigo=${codigoRespuesta}&tipo=RESPUESTA`;
             this.enlaceResultados = `http://localhost:4200/encuestas/${id}?codigo=${codigoResultados}&tipo=RESULTADOS`;
+        } else {
+            this.router.navigate(['/']);
         }
     }
-
 
     copiarTexto(id: string) {
         const texto = document.getElementById(id)?.textContent;
